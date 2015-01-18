0bade7ab216199efee5586a5fcbd68b4d3c7deae
Emily Carpenetti <xiomerra@gmail.com>
2015-01-17 20:51:27 -0800

    set up with VS2013 for editing on pc, restructured directory

---
+++ source/client.cpp
@@ -0,0 +1,13 @@
+#ifdef XMJ_CLIENT
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World from XMJ client!" << endl;
+
+  return 0;
+}
+
+#endif
---
+++ source/server.cpp
@@ -0,0 +1,16 @@
+//server.cpp
+//created 1/16/2015
+//entry point for the server program code
+
+#ifdef XMJ_SERVER
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello world from XMJ server!";
+	return 0;
+}
+
+#endif
